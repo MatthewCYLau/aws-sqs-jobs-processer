c4e8ae77fcad04d49dda039fae3852817d0afbe5
Matthew Lau <lau.cy.matthew@gmail.com>
2021-03-17 16:42:20 +0000

    Updated Lambda function

--- deploy/lambdas/processQueue/index.js
+++ deploy/lambdas/processQueue/index.js
@@ -1,6 +1,6 @@
 // Load the AWS SDK for Node.js
 const { DynamoDBClient, PutItemCommand } = require("@aws-sdk/client-dynamodb");
-const axios = require("axios");
+// const axios = require("axios");
 
 // Set the region
 const REGION = "us-east-1";
@@ -8,24 +8,33 @@ const REGION = "us-east-1";
 // Create DynamoDB service object
 const dbclient = new DynamoDBClient({ region: REGION });
 
+const uuidv4 = () =>
+  "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
+    var r = (Math.random() * 16) | 0,
+      v = c == "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+
 exports.handler = async event => {
-  const res = await axios.get("https://jsonplaceholder.typicode.com/todos/1");
+  // const res = await axios.get("https://jsonplaceholder.typicode.com/todos/1");
   const promises = event.Records.map(record => {
     const body = JSON.parse(record.body);
     const size = body.data.length;
-    const todo = {
-      M: {
-        id: {
-          S: res.data.id.toString()
-        },
-        todo: {
-          S: res.data.title
+    const createTodo = todo => {
+      return {
+        M: {
+          id: {
+            S: uuidv4()
+          },
+          todo: {
+            S: todo
+          }
         }
-      }
+      };
     };
     let todos = [];
     for (let counter = 0; counter < size; counter++) {
-      todos.push(todo);
+      todos.push(createTodo(body.data[counter]));
     }
     const params = {
       TableName: "jobs",
