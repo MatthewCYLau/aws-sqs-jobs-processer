61f3b1e07e2887e0dd2e82d196d1680e866ddc14
Matthew Lau <lau.cy.matthew@gmail.com>
2021-03-24 23:34:06 +0000

    Added secondary index to DynamoDB

--- deploy/06-dynamodb.tf
+++ deploy/06-dynamodb.tf
@@ -2,11 +2,23 @@ resource "aws_dynamodb_table" "jobs" {
   name             = "jobs"
   billing_mode     = "PAY_PER_REQUEST"
   hash_key         = "jobId"
+  range_key        = "created"
   stream_enabled   = true
   stream_view_type = "NEW_AND_OLD_IMAGES"
 
   attribute {
     name = "jobId"
     type = "S"
+  }
+
+  attribute {
+    name = "created"
+    type = "S"
+  }
+
+  local_secondary_index {
+    name            = "byCreated"
+    projection_type = "ALL"
+    range_key       = "created"
   }
 }
