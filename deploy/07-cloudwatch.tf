1c380daa180f5c7d309dab115ccb3d414c86f5a3
Matthew Lau <lau.cy.matthew@gmail.com>
2021-03-25 20:56:53 +0000

    Add CloudWatch Alarm

---
+++ deploy/07-cloudwatch.tf
@@ -0,0 +1,15 @@
+resource "aws_cloudwatch_metric_alarm" "process_queue" {
+  alarm_name          = "process-queue-alarm"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "Errors"
+  namespace           = "AWS/Lambda"
+  period              = "60"
+  statistic           = "Average"
+  threshold           = "1"
+  alarm_description   = "Lambda error metrics"
+  dimensions = {
+    FunctionName = aws_lambda_function.process_queue.function_name
+  }
+  alarm_actions = ["arn:aws:sns:us-east-1:830663695860:Default_CloudWatch_Alarms_Topic"]
+}
